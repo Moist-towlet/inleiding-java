9016394b1475877cbd480277779ac74b8e0f822f
sjors <sjorsdeboer13@gmail.com>
2020-03-10 10:35:26 +0100

    Hoofstuk 4 opdracht 2 Huis gemaakt

--- src/Ho4/Huis.java
+++ src/Ho4/Huis.java
@@ -5,12 +5,16 @@ import java.applet.*;
 public class Huis extends Applet {
 
     public void init() {
-        setBackground(Color.LIGHT_GRAY);
+        setBackground(Color.pink);
     }
 
     public void paint(Graphics g) {
-        g.setColor(Color.white);
-        g.drawRect(25, 50, 200, 300);
-        g.drawRect(20, 20, 100, 100);
+        g.setColor(Color.gray);
+        g.fillRect(15, 40, 300, 300);
+        g.setColor(Color.cyan);
+        g.fillRect(30, 77, 75, 75);
+        g.fillRect(115, 77, 75, 75);
+        g.setColor(Color.black);
+        g.fillRect(200, 150, 75, 150);
     }
 }
