55ed3a398f4508c129540469a79063bbf78edfae
sjors <sjorsdeboer13@gmail.com>
2020-03-10 09:51:41 +0100

    Hoofstuk 4 opdracht 2 huis maken

--- src/Ho4/Huis.java
+++ src/Ho4/Huis.java
@@ -1,17 +1,16 @@
 package Ho4;
-
 import java.awt.*;
 import java.applet.*;
 
-public class Show extends Applet {
+public class Huis extends Applet {
 
     public void init() {
         setBackground(Color.LIGHT_GRAY);
     }
 
     public void paint(Graphics g) {
-        g.setColor(Color.blue);
-        g.drawLine(50, 250, 250, 250);
-
+        g.setColor(Color.white);
+        g.drawRect(25, 50, 200, 300);
+        g.drawRect(20, 20, 100, 100);
     }
 }
