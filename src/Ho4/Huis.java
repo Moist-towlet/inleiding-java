3c0f4ab6b769f51cbfae8c699cbbec39ab87c3b8
sjors <sjorsdeboer13@gmail.com>
2020-03-10 13:04:35 +0100

    Hoofstuk 4 opdracht 3 Vlag

--- src/Ho4/Huis.java
+++ src/Ho4/Huis.java
@@ -8,13 +8,32 @@ public class Huis extends Applet {
         setBackground(Color.pink);
     }
 
+    //Note to self: moet leren beter stuff in te delen
+
     public void paint(Graphics g) {
         g.setColor(Color.gray);
-        g.fillRect(15, 40, 300, 300);
+        g.fillRect(5, 40, 300, 300);
         g.setColor(Color.cyan);
         g.fillRect(30, 77, 75, 75);
         g.fillRect(115, 77, 75, 75);
         g.setColor(Color.black);
         g.fillRect(200, 150, 75, 150);
+        //Code voor de vlag
+        g.setColor(Color.darkGray);
+        g.fillRect(325, 40, 5, 300);
+        g.setColor(Color.red);
+        g.fillRect(330, 40, 45, 20);
+        g.setColor(Color.white);
+        g.fillRect(330, 60, 45, 20);
+        g.setColor(Color.blue);
+        g.fillRect(330, 80, 45, 20);
+        g.setColor(Color.red);
+        g.fillRect(375, 50, 45, 20);
+        g.setColor(Color.white);
+        g.fillRect(375, 70, 45, 20);
+        g.setColor(Color.blue);
+        g.fillRect(375, 90, 45, 20);
+
+
     }
 }
