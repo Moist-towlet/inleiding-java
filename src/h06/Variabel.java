129af0ed378f149c7e0a16d049d48e9c6cc26c04
sjors <sjorsdeboer13@gmail.com>
2020-03-31 17:16:15 +0200

    Hoofstuk 6 experimenteren met de variabelen

--- src/h06/Variabel.java
+++ src/h06/Variabel.java
@@ -8,16 +8,24 @@ import java.applet.*;
 
 public class Variabel extends Applet {
     double a, b, c, uitkomst;
-
+    double scoredouble;
+    int scoreint;
 
     public void init() {
+        scoredouble = 44.8;
+        scoreint = (int) scoredouble;
         a = 8;
         b = 8;
         c = 3;
         uitkomst = (a + b) / c;
+
+
+
     }
 
     public void paint(Graphics g) {
+
         g.drawString("De uitkomst is: " + uitkomst, 20, 20);
+        g.drawString("De convertie is " + scoreint, 20, 30);
     }
 }
